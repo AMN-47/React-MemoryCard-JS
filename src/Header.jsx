f6ab36c05c4de068f876811ad7744a64f92f2156
AMN-47 <akhilnagar57@gmail.com>
2026-04-23 18:31:06 +0200

    Created header html with header class

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import './Header.css';
 
 export default function Header({ score, bestScore, total }) {
-    
+    return(
+        <header className = "header">
+            
+        </header>
+    );
 }
