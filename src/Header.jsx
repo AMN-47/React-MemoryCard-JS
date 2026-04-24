84f33deb56d06baada4b4e698e0f32d648f03c06
AMN-47 <akhilnagar57@gmail.com>
2026-04-24 19:10:34 +0200

    Completed heading and subtitle of div

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,6 +6,10 @@ export default function Header({ score, bestScore, total }) {
         <header className = "header">
             <div className = "header__brand">
                 <span className="header__pokeball" aria-hidden="true"/>
+                <div>
+                    <h1 className="header__title">Memory Game</h1>
+                    <p className ="header__subtitle">Dont click the same Pokémon twice!</p>
+                </div>
             </div>
         </header>
     );
