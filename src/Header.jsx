c617867d116a7a9b8f5bf3454456173566d5231c
AMN-47 <akhilnagar57@gmail.com>
2026-04-24 19:08:51 +0200

    Adding div for heading

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,9 @@ import './Header.css';
 export default function Header({ score, bestScore, total }) {
     return(
         <header className = "header">
-            
+            <div className = "header__brand">
+                <span className="header__pokeball" aria-hidden="true"/>
+            </div>
         </header>
     );
 }
