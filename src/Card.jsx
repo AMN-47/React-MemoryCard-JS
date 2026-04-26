cd953d07e4aed838cd2a71f5a6bf65cb4911ef1a
AMN-47 <akhilnagar57@gmail.com>
2026-04-26 19:02:36 +0200

    Added variables for variety of pokemon colors

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,28 @@
 import React, { useState } from 'react';
 import './Card.css';
 
-export default function Card({ id, name, image, types, onClick }) {
+const TYPE_COLORS = {
+  fire: '#FF6B35',
+  water: '#4A9ECC',
+  grass: '#5DAA59',
+  electric: '#F6C90E',
+  psychic: '#E8708A',
+  ice: '#74CEC0',
+  dragon: '#6F55C8',
+  dark: '#4A3B5C',
+  fairy: '#F0A0C0',
+  normal: '#A8A878',
+  fighting: '#C03028',
+  flying: '#A890F0',
+  poison: '#A040A0',
+  ground: '#E0C068',
+  rock: '#B8A038',
+  bug: '#A8B820',
+  ghost: '#705898',
+  steel: '#B8B8D0',
+};
 
+
+export default function Card({ id, name, image, types, onClick }) {
+    const [flipping, setFlipping] = useState(false);
 }
