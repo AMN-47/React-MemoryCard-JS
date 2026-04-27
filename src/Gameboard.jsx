374d7d5aa07231d49e0ed7e324baf2851d83b10e
AMN-47 <akhilnagar57@gmail.com>
2026-04-27 20:11:13 +0200

    Created div for grid, mapped card paremeter

--- src/Gameboard.jsx
+++ src/Gameboard.jsx
@@ -3,5 +3,14 @@ import Card from './Card'
 import './Gameboard.css'
 
 export default function GameBoard({cards, onCardClick}) {
-
+    return(
+        <main className='gameboard'>
+            <div className='gameboard__grid'>
+                {cards.map(card => (
+                    <Card
+                    />
+                ))}
+            </div>
+        </main>
+    )
 }
